033720f86c756eca272fe24ba8b205fbd3c9f077
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-15 13:52:45 -0700

    Fix image

--- my_model/_image.py
+++ my_model/_image.py
@@ -1,3 +1,3 @@
 from my_model import __version__# DOCKERHUB IMAGE
-IMAGE = f"registry.hub.docker.com/jgarra/my-model:{__version__}"
+IMAGE = f"registry.hub.docker.com/jgarrahan/my-model:{__version__}"
 
