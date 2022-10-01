af3793c5d6a15fbff81ab1c0d11b2da8c4662e18
Jacqueline Garrahan <jacquelinegarrahan@gmail.com>
2022-09-30 22:19:03 -0700

    Initial checkin

--- my_model/model.py
+++ my_model/model.py
@@ -1,28 +1,27 @@
 import copy
 from typing import Dict
+import numpy as np
 from lume_model.models import BaseModel
 from lume_model.variables import InputVariable, OutputVariable
-
 from my_model import INPUT_VARIABLES, OUTPUT_VARIABLES
-
-
 
 class MyModel(BaseModel):
     input_variables = copy.deepcopy(INPUT_VARIABLES)
     output_variables = copy.deepcopy(OUTPUT_VARIABLES)
 
     def __init__(self, **settings_kwargs):
-        """Initialize the model. If additional settings are required, they can be 
+        """Initialize the model. If additional settings are required, they can be
         passed and handled here. For models that are wrapping model loads
         from other frameworks, this can be used for loading weights, referencing
         data files, etc.
-        
+
         """
-        super().__init__()
+        super(self).__init__()
 
         # handle settings if any
         # if settings_kwargs is not None:
         # ...
+
 
     def evaluate(
         self, input_variables: Dict[str, InputVariable]
@@ -40,6 +39,13 @@ class MyModel(BaseModel):
 
         """
 
-        ...
+        self.output_variables["output1"].value = np.random.uniform(
+            input_variables["input1"].value,  # lower dist bound
+            input_variables["input2"].value,  # upper dist bound
+            (50, 50),
+        )
+        self.output_variables["output2"].value = input_variables["input1"].value
+        self.output_variables["output3"].value = input_variables["input2"].value
+
 
         return self.output_variables
